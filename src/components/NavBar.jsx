375470e0df19af48dc7690ea5463312191ba3072
duong-28 <thuyduong.kt.ftu@gmail.com>
2024-12-15 19:23:18 +1000

    navbar styled woo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,46 @@
 import { Link, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/use-auth.js";
+import "./NavBar.css";
 
 function NavBar() {
-  const { auth, setAuth } = useAuth();
+    const { auth, setAuth } = useAuth();
 
-  const handleLogout = () => {
-    window.localStorage.removeItem("token")
-    setAuth({ token: null, userId: null }); 
-  };
-  
-  console.log(auth);
+    const handleLogout = () => {
+        window.localStorage.removeItem("token")
+        setAuth({ token: null, userId: null });
+    };
 
-  // debugger;
-
-  return (
-    <div>
-      <nav>
-        {/* Put them in a list */}
-        <Link to="/">Home</Link>
-        {/* <Link to="/login">Log In</Link> */}
-        {auth.token? (<Link to="/" onClick={handleLogout}>Log Out</Link>) : (<Link to="/login">Log In</Link>)}
-        {auth.userId? <Link to="/user-page">User Dashboard</Link> : null}
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/create-project">Create Project</Link>
-      </nav>
-      <Outlet />
-    </div>
-  );
+    return (
+        <div>
+            <nav>
+                <div className="nav-left">
+                    <Link to="/" className="logo">
+                        <img src="/photos/Logo.png" alt="Atletico Saigon Logo" className="nav-logo-img" />
+                        Atletico Saigon
+                    </Link>
+                </div>
+                <div className="nav-center">
+                    <Link to="/" className="nav-item">Home</Link>
+                    <Link to="/projects" className="nav-item">Projects</Link>
+                    <Link to="/about" className="nav-item">About</Link>
+                </div>
+                <div className="nav-right">
+                    {auth.token ? (
+                        <>
+                            <Link to="/create-project" className="nav-item">Create Project</Link>
+                            <Link to="/" onClick={handleLogout} className="nav-item">Log Out</Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/login" className="nav-item login">Log In</Link>
+                            <Link to="/signup" className="nav-item signup">Sign Up</Link>
+                        </>
+                    )}
+                </div>
+            </nav>
+            <Outlet />
+        </div>
+    );
 }
 
 export default NavBar;
