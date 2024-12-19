64ed0a73f76aa99682e18dc11a741d0fd5bacbc0
duong-28 <thuyduong.kt.ftu@gmail.com>
2024-12-19 22:12:31 +1000

    login and signup forms styled

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,7 +1,18 @@
 import SignUpForm from "../components/SignUpForm";
+import "../styles/AuthPages.css";
 
 function SignUpPage() {
-    return <SignUpForm />;
-  }
-  
-  export default SignUpPage;
+    return (
+        <div className="auth-page">
+            <div className="auth-container">
+                <div className="auth-header">
+                    <h1>Join Atletico</h1>
+                    <p>Start making a difference in youth soccer</p>
+                </div>
+                <SignUpForm />
+            </div>
+        </div>
+    );
+}
+
+export default SignUpPage;
