1c45e214ca7a51be74d2731c6614068b20aee5db
duong-28 <thuyduong.kt.ftu@gmail.com>
2024-12-23 23:17:42 +1000

    fixed error message for UpdateUserForm and complete UserPage styling

--- src/api/update-user.js
+++ src/api/update-user.js
@@ -1,27 +1,28 @@
 async function updateUser(userId, userData, token) {
     const url  = `${import.meta.env.VITE_API_URL}/users/${userId}/`;
     
-    const response = await fetch(url, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            "Authorization": `Token ${token}`,
-        },
-        body: JSON.stringify(userData),
-    });
-
-    if (!response.ok) {
-        const fallbackError = `Failed to update user ${userData.name}`;
-
-        const data = await response.json().catch(() => {
-            throw new Error(fallbackError);
+    try {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": `Token ${token}`,
+            },
+            body: JSON.stringify(userData),
         });
 
-        const errorMessage = data?.detail ?? fallbackError;
-        throw new Error(errorMessage);
-    }
+        if (!response.ok) {
+            const data = await response.json();
+            throw {
+                message: "Failed to update user",
+                response: { data }
+            };
+        }
 
-    return await response.json();
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
 }
 
 export default updateUser;
